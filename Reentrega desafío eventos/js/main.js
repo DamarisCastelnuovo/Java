871967fb7fa7a743a9178a6a365b2ed73708da3b
DamarisCastelnuovo <104535942+DamarisCastelnuovo@users.noreply.github.com>
2022-09-25 21:04:29 -0300

    Add files via upload

---
+++ Reentrega desafío eventos/js/main.js
@@ -0,0 +1,12 @@
+const formulario = document.querySelector("form")
+const nombre = document.querySelector("#formName")
+const direccion = document.querySelector("#formDireccion")
+const mail = document.querySelector("#formEmail")
+const boton = document.querySelector(".btn")
+
+boton.addEventListener("click", (e)=>{
+    e.preventDefault()
+    let btnEnviar = document.createElement("div");
+    btnEnviar.innerHTML = `<h4 class="form_env"> Muchas gracias ${nombre.value} por su compra, sus productos se enviaran a: ${direccion.value}. Recibirá nuestras mejores ofertas y novedades en ${mail.value}. </h4>`;
+    document.body.appendChild(btnEnviar);
+})
